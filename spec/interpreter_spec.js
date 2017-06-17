ff86ea1da1a0bd5d522af8ae39358e9e18e77171
John B. Lee <j.benlin.lee@gmail.com>
2017-06-17 18:04:28 -0400

    Interpreter precedence handling tests

--- spec/interpreter_spec.js
+++ spec/interpreter_spec.js
@@ -25,12 +25,35 @@ describe('Interpreter', () => {
   });
 
   describe('when handling expressions where precedence matters', () => {
-    it.skip('should multiply before adding', () => {});
-    it.skip('should multiply before subtracting', () => {});
-    it.skip('should divide before adding', () => {});
-    it.skip('should divide before subtracting', () => {});
-    it.skip('should add and subtract from left to right', () => {});
-    it.skip('should multiply and divide from left to right', () => {});
+    it('should multiply before adding', () => {
+      const interpreter = new Interpreter("4 + 5 * 3"); // Should be 19 and not 27
+      assert.equal(interpreter.eval(), 19);
+    });
+
+    it('should multiply before subtracting', () => {
+      const interpreter = new Interpreter("15 - 3 * 2"); // Should be 9 and not 24
+      assert.equal(interpreter.eval(), 9);
+    });
+
+    it('should divide before adding', () => {
+      const interpreter = new Interpreter("20 + 12 / 3"); // Should be 24 and not (32 / 3)
+      assert.equal(interpreter.eval(), 24);
+    });
+
+    it('should divide before subtracting', () => {
+      const interpreter = new Interpreter("4 - 6 / 2"); // Should be 1 and not -1
+      assert.equal(interpreter.eval(), 1);
+    });
+
+    it('should add and subtract from left to right', () => {
+      const interpreter = new Interpreter("4 + 5 - 2");
+      assert.equal(interpreter.eval(), 7);
+    });
+
+    it('should multiply and divide from left to right', () => {
+      const interpreter = new Interpreter("12 * 2 / 3");
+      assert.equal(interpreter.eval(), 8);
+    });
   });
 
   describe('when handling expressions with parentheses', () => {
