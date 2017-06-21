7adf4c067441bfd5f3a079a07bfdee3f4a78e5c8
John B. Lee <j.benlin.lee@gmail.com>
2017-06-20 23:42:15 -0400

    Move interpreter stuff back into interpreter and out of AST.

--- interpreter.js
+++ interpreter.js
@@ -3,11 +3,46 @@ const Parser = require('./parser.js');
 const AST = require('./ast.js');
 
 module.exports = class Interpreter {
+  static visit_BINOP(node) {
+    const lhs = Interpreter.evalTree(node.left);
+    const rhs = Interpreter.evalTree(node.right);
+
+    switch (node.op.type) {
+      case Lexer.PLUS:
+        return lhs + rhs;
+      case Lexer.MINUS:
+        return lhs - rhs;
+      case Lexer.MULTIPLY:
+        return lhs * rhs;
+      case Lexer.DIVIDE:
+        return lhs / rhs;
+    }
+  }
+
+  static visit_INTEGER(node) {
+    return node.val;
+  }
+
+  static visit_UNARYOP(node) {
+    let factor = 1;
+    if (node.op.type === Lexer.MINUS) {
+      factor = -1;
+    }
+
+    return factor * Interpreter.evalTree(node.expr);
+  }
+
+  /* Evaluates AST starting from a root node */
+  static evalTree(node) {
+    const visitor = Interpreter[`visit_${node.type}`];
+    return visitor.call(this, node);
+  }
+
   static eval(stmt) {
     const parser = new Parser(stmt);
     const astree = parser.parse();
 
     // Use the AST to calculate the final result
-    return AST.Visitor.eval(astree);
+    return Interpreter.evalTree(astree);
   }
 }
