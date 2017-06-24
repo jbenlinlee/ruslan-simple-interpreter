3059590efcdc1f92589bc546dd6c26c63aa9b281
John B. Lee <j.benlin.lee@gmail.com>
2017-06-23 21:31:08 -0400

    Allow variables to start with an underscore

--- lexer.js
+++ lexer.js
@@ -102,7 +102,8 @@ module.exports.Lexer = class Lexer {
     const code = char.charCodeAt(0);
     if (!(code > 47 && code < 58) && // numeric (0-9)
         !(code > 64 && code < 91) && // upper alpha
-        !(code > 96 && code < 123)) { // lower alphanumeric
+        !(code > 96 && code < 123) && // lower alphanumeric
+        !(code == 95)) { // underscore
           return false;
     }
 
