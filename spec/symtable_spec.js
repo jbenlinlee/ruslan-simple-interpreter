e2e97208ed7142b0bd88acf0c8283ab41895cee2
John B. Lee <j.benlin.lee@gmail.com>
2017-06-27 21:40:36 -0400

    Add symbol table classes and specs

---
+++ spec/symtable_spec.js
@@ -0,0 +1,50 @@
+const Symtable = require('../lib/symtable.js');
+const Parser = require('../lib/parser.js');
+const AST = require('../lib/ast.js');
+const assert = require('assert');
+
+describe('Symbol Table', () => {
+  let tbl = undefined;
+
+  beforeEach(() => {
+    tbl = new Symtable.SymbolTable();
+  });
+
+  it('should return INTEGER builtin type symbol', () => {
+    const intBuiltin = tbl.lookup(AST.NodeTypes.INTEGER);
+    assert.equal(intBuiltin instanceof Symtable.BuiltinTypeSymbol, true);
+    assert.equal(intBuiltin.name, AST.NodeTypes.INTEGER);
+  });
+
+  it('should return REAL builtin type symbol', () => {
+    const realBuiltin = tbl.lookup(AST.NodeTypes.REAL);
+    assert.equal(realBuiltin instanceof Symtable.BuiltinTypeSymbol, true);
+    assert.equal(realBuiltin.name, AST.NodeTypes.REAL);
+  });
+});
+
+describe('Symbol Table Builder', () => {
+  let builder = undefined;
+
+  beforeEach(() => {
+    builder = new Symtable.SymbolTableBuilder();
+  });
+
+  it('should return true for a program with valid assignments', () => {
+    const node = Parser.parseProgram('PROGRAM test; VAR a : INTEGER; BEGIN a := 5; END.');
+    const isValid = builder.visit(node);
+    assert.equal(isValid, true);
+  });
+
+  it('should return false for a program with an invalid assignment LHS', () => {
+    const node = Parser.parseProgram('PROGRAM test; VAR a : INTEGER; BEGIN b := 5; END.');
+    const isValid = builder.visit(node);
+    assert.equal(isValid, false);
+  });
+
+  it('should return false for a program with an invalid assignment RHS', () => {
+    const node = Parser.parseProgram('PROGRAM test; VAR a : INTEGER; BEGIN a := b; END.');
+    const isValid = builder.visit(node);
+    assert.equal(isValid, false);
+  });
+});
