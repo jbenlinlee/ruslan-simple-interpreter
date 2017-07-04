705c9ae51de2e11e1e69a8dae48160c7b244c4b3
John B. Lee <j.benlin.lee@gmail.com>
2017-07-04 09:17:28 -0400

    Clean up semantic analyzer spec

--- spec/semantic_analyzer_spec.js
+++ spec/semantic_analyzer_spec.js
@@ -9,73 +9,79 @@ describe('Semantic Analyzer', () => {
     builder = new SemanticAnalyzer();
   });
 
-  it('should return true for a program with valid assignments', () => {
-    const node = Parser.parseProgram('PROGRAM test; VAR a : INTEGER; BEGIN a := 5; END.');
-    const isValid = builder.visit(node);
-    assert.equal(isValid, true);
+  describe('when handling assignments', () => {
+    it('should return true for a program with valid assignments', () => {
+      const node = Parser.parseProgram('PROGRAM test; VAR a : INTEGER; BEGIN a := 5; END.');
+      const isValid = builder.visit(node);
+      assert.equal(isValid, true);
+    });
+
+    it('should return false for a program with an invalid assignment LHS', () => {
+      const node = Parser.parseProgram('PROGRAM test; VAR a : INTEGER; BEGIN b := 5; END.');
+      const isValid = builder.visit(node);
+      assert.equal(isValid, false);
+    });
+
+    it('should return false for a program with an invalid assignment RHS', () => {
+      const node = Parser.parseProgram('PROGRAM test; VAR a : INTEGER; BEGIN a := b; END.');
+      const isValid = builder.visit(node);
+      assert.equal(isValid, false);
+    });
   });
 
-  it('should return false for a program with an invalid assignment LHS', () => {
-    const node = Parser.parseProgram('PROGRAM test; VAR a : INTEGER; BEGIN b := 5; END.');
-    const isValid = builder.visit(node);
-    assert.equal(isValid, false);
+  describe('when handling declarations', () => {
+    it('should return true for a program with a procedure declaration', () => {
+      const node = Parser.parseProgram('PROGRAM test; PROCEDURE myproc; BEGIN END; BEGIN END.');
+      const isValid = builder.visit(node);
+      assert.equal(isValid, true);
+    });
+
+    it('should return false for a program with a var defined twice', () => {
+      const node = Parser.parseProgram('PROGRAM test; VAR x : INTEGER; x : REAL; BEGIN END.');
+      const isValid = builder.visit(node);
+      assert.equal(isValid, false);
+    });
+
+    it('should return true for a program with a procedure that uses a local scoped var', () => {
+      const node = Parser.parseProgram('PROGRAM test; PROCEDURE myproc; VAR x : INTEGER; BEGIN x := 5; END; BEGIN END.');
+      const isValid = builder.visit(node);
+      assert.equal(isValid, true);
+    });
+
+    it('should return true for a program with a procedure that uses a procedure param', () => {
+      const node = Parser.parseProgram('PROGRAM test; PROCEDURE myproc(a : INTEGER); VAR x : INTEGER; BEGIN x := a; END; BEGIN END.');
+      const isValid = builder.visit(node);
+      assert.equal(isValid, true);
+    });
   });
 
-  it('should return false for a program with an invalid assignment RHS', () => {
-    const node = Parser.parseProgram('PROGRAM test; VAR a : INTEGER; BEGIN a := b; END.');
-    const isValid = builder.visit(node);
-    assert.equal(isValid, false);
-  });
+  describe('when handling variable refs and scoping', () => {
+    it('should return true for a program with a procedure that refers to a var in parent scope', () => {
+      const node = Parser.parseProgram('PROGRAM test; VAR x : REAL; PROCEDURE myproc; VAR a : REAL; BEGIN a := x; END; BEGIN END.');
+      const isValid = builder.visit(node);
+      assert.equal(isValid, true);
+    });
 
-  it('should return true for a program with a procedure declaration', () => {
-    const node = Parser.parseProgram('PROGRAM test; PROCEDURE myproc; BEGIN END; BEGIN END.');
-    const isValid = builder.visit(node);
-    assert.equal(isValid, true);
-  });
+    it('should return true even if a local scoped var masks a parent scoped var', () => {
+      const node = Parser.parseProgram('PROGRAM test; VAR x : REAL; PROCEDURE myproc; VAR x : INTEGER; BEGIN END; BEGIN END.');
+      const isValid = builder.visit(node);
+      assert.equal(isValid, true);
+    });
 
-  it('should return false for a program with a var defined twice', () => {
-    const node = Parser.parseProgram('PROGRAM test; VAR x : INTEGER; x : REAL; BEGIN END.');
-    const isValid = builder.visit(node);
-    assert.equal(isValid, false);
-  });
+    it('should return false for a program that tries to refer to a var outside of scope', () => {
+      const node = Parser.parseProgram('PROGRAM test; VAR x : REAL; PROCEDURE myproc; VAR a : REAL; BEGIN END; BEGIN x := a; END.');
+      const isValid = builder.visit(node);
+      assert.equal(isValid, false);
+    });
 
-  it('should return true for a program with a procedure that uses a local scoped var', () => {
-    const node = Parser.parseProgram('PROGRAM test; PROCEDURE myproc; VAR x : INTEGER; BEGIN x := 5; END; BEGIN END.');
-    const isValid = builder.visit(node);
-    assert.equal(isValid, true);
-  });
+    it('should create a symbol table containing INTEGER and REAL builtins as well as program name', () => {
+      const node = Parser.parseProgram('PROGRAM test; BEGIN END.');
+      const isValid = builder.visit(node);
 
-  it('should return true for a program with a procedure that uses a procedure param', () => {
-    const node = Parser.parseProgram('PROGRAM test; PROCEDURE myproc(a : INTEGER); VAR x : INTEGER; BEGIN x := a; END; BEGIN END.');
-    const isValid = builder.visit(node);
-    assert.equal(isValid, true);
-  });
-
-  it('should return true for a program with a procedure that refers to a var in parent scope', () => {
-    const node = Parser.parseProgram('PROGRAM test; VAR x : REAL; PROCEDURE myproc; VAR a : REAL; BEGIN a := x; END; BEGIN END.');
-    const isValid = builder.visit(node);
-    assert.equal(isValid, true);
-  });
-
-  it('should return true even if a local scoped var masks a parent scoped var', () => {
-    const node = Parser.parseProgram('PROGRAM test; VAR x : REAL; PROCEDURE myproc; VAR x : INTEGER; BEGIN END; BEGIN END.');
-    const isValid = builder.visit(node);
-    assert.equal(isValid, true);
-  });
-
-  it('should return false for a program that tries to refer to a var outside of scope', () => {
-    const node = Parser.parseProgram('PROGRAM test; VAR x : REAL; PROCEDURE myproc; VAR a : REAL; BEGIN END; BEGIN x := a; END.');
-    const isValid = builder.visit(node);
-    assert.equal(isValid, false);
-  });
-
-  it('should create a symbol table containing INTEGER and REAL builtins as well as program name', () => {
-    const node = Parser.parseProgram('PROGRAM test; BEGIN END.');
-    const isValid = builder.visit(node);
-
-    assert.equal(isValid, true);
-    assert.notStrictEqual(builder.rootScope.lookup('INTEGER'), undefined);
-    assert.notStrictEqual(builder.rootScope.lookup('REAL'), undefined);
-    assert.notStrictEqual(builder.rootScope.lookup('test'), undefined);
+      assert.equal(isValid, true);
+      assert.notStrictEqual(builder.rootScope.lookup('INTEGER'), undefined);
+      assert.notStrictEqual(builder.rootScope.lookup('REAL'), undefined);
+      assert.notStrictEqual(builder.rootScope.lookup('test'), undefined);
+    });
   });
 });
